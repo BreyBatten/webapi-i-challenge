806c27767db9bf7f4af67512e660506945f6bda0
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-07-24 23:20:39 -0400

    set up project, installed nodemon and express, began writing out endpoints for GET

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+//GET
+server.get("/api/users", (req, res) => {
+    db.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The users information could not be retrieved." })
+    })
+});
+
+server.get("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+
+    db.findById()
+    .then(user => {
+        if(user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist."
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: "The user information could not be retrieved."
+        })
+    })
+})
+
+//POST
+server.post("/api/users", (req, res) => {
+    
+})
+
+server.listen(4000, () => {
+    console.log("server listening on port 4000");
+});
