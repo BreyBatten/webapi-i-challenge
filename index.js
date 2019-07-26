a191f9b0a1ee27b9f373f1aadbe645b57d7af0c8
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-07-25 20:46:50 -0400

    built out delete, started put

--- index.js
+++ index.js
@@ -39,8 +39,7 @@ server.get("/api/users/:id", (req, res) => {
 
 //POST
 server.post("/api/users", (req, res) => {
-    const userInfo = req.body;
-    const { name, bio } = userInfo;
+    const { name, bio } = req.body;
 
     db.insert(name && bio)
     .then(user => {
@@ -49,8 +48,34 @@ server.post("/api/users", (req, res) => {
     .catch(err => {
         res.status(400).json({ success: false, err, error: "Please provide name and bio for the user."})
     })
+});
+
+//DELETE
+server.delete("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+
+    db.remove(id)
+    .then(deleted => {
+        if(deleted) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({ success: false, message: "The user with the specified ID does not exist."})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ success: false, error: "The user could not be removed."})
+    })
+});
+
+//PUT
+server.put("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, bio } = req.body;
+    const userInfo = req.body;
+
+    db.update()
 })
 
-server.listen(4000, () => {
-    console.log("server listening on port 4000");
+server.listen(5000, () => {
+    console.log("server listening on port 5000");
 });
